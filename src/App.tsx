5f1cbe291279549f011bf19a313bc7c2d4e0bd8f
grafika10 <grafika10@ucione.local>
2024-03-13 21:01:16 +0100

    Add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Header from "./components/header";
+
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <>
+      <h1 className="header">Title</h1>
+    </>
+  );
+};
+
+export default Header;
