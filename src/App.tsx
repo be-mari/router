83e50fc45a7bde1499e2567c4c74727b822a040d
grafika10 <be.marija@gmail.com>
2024-03-13 21:57:21 +0100

    nedovršeno

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import Layout from "./components/layout";
 import Header from "./components/header";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
 
 const App = () => {
   return (
     <>
       <Layout>
         <h1>Nešto</h1>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="*" element={<NoMatch />} />
+          </Route>
+        </Routes>
       </Layout>
       <Header />
     </>
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from "react";
 
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+
 type Props = {
   children: ReactNode;
 };
@@ -7,7 +9,33 @@ type Props = {
 const Layout = ({ children }: Props) => {
   return (
     <>
-      <div className="layout">{children}</div>
+      <div className="layout">
+        {/* A "layout route" is a good place to put markup you want to
+          share across all the pages on your site, like navigation. */}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/nothing-here">Nothing Here</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <hr />
+
+        {/* An <Outlet> renders whatever child route is currently active,
+          so you can think about this <Outlet> as a placeholder for
+          the child routes we defined above. */}
+        <Outlet />
+      </div>
     </>
   );
 };
