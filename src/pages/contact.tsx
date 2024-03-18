fd381887d62b40f431dc45af7f46459cfecd907d
grafika10 <be.marija@gmail.com>
2024-03-18 21:30:49 +0100

    Flags

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,4 +1,35 @@
+import { useEffect, useState } from "react";
+
+type CountryType = {
+  flags: {
+    png: string;
+  };
+};
+
 const Contact = () => {
-  return <>I'm Contact</>;
+  const [country, setCountry] = useState<CountryType | undefined>(undefined);
+
+  const getData = async () => {
+    await fetch(`https://restcountries.com/v3.1/name/spain`)
+      .then((data) => {
+        return data.json();
+      })
+      .then((res: CountryType[]) => {
+        setCountry(res[0]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <>
+      <img src={country?.flags.png} alt="" />
+    </>
+  );
 };
 export default Contact;
